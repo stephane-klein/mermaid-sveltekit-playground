353426799e36334b31d3a735ffc6ac1dbd53bfa3
stephane-klein <contact@stephane-klein.info>
2024-05-06 15:54:52 +0200

    Add flowchart and href link in node

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,15 +3,16 @@
     import mermaid from "mermaid";
 
     let diagram = `\
-erDiagram
-    CUSTOMER }|..|{ DELIVERY-ADDRESS : has
-    CUSTOMER ||--o{ ORDER : places
-    CUSTOMER ||--o{ INVOICE : "liable for"
-    DELIVERY-ADDRESS ||--o{ ORDER : receives
-    INVOICE ||--|{ ORDER : covers
-    ORDER ||--|{ ORDER-ITEM : includes
-    PRODUCT-CATEGORY ||--|{ PRODUCT : contains
-    PRODUCT ||--o{ ORDER-ITEM : "ordered in"`;
+flowchart LR
+    id1[Node1]
+    id2[Node2]
+    id3[Node3]
+    id4[Node 4 with <a href="https://github.com/stephane-klein/mermaid-sveltekit-playground">link</a>]
+    id5[Node5]
+    id1 --> id2
+    id1 --> id3
+    id2 --> id4 --> id5
+    `;
 
     let container;
 
